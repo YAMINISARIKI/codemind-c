e8dce918f1b81270564c928cdff4d033edfcf7f2
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-02-15 22:24:31 +0530

    #include<stdio.h> int main() { 	int r,c; 	scanf("%d%d",&r,&c); 	int a[r][c],i,j,colmax=a[0][0]; 	for(i=0;i<r;i++) 	{ 		for(j=0;j<c;j++) 		{ 			scanf("%d",&a[i][j]); 		} 	} 	for(j=0;j<c;j++) 	{ 		colmax = a[0][0]; 		for(i=0;i<r;i++) 		{ 			if(colmax<a[i][j]) 			colmax=a[i][j]; 		//	colsum=colsum+a[j][i]; 		} 		printf("%d",colmax);     	printf(" "); 	} }

---
+++ Maximum_element_in_each_column.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int r,c;
+	scanf("%d%d",&r,&c);
+	int a[r][c],i,j,colmax=a[0][0];
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(j=0;j<c;j++)
+	{
+		colmax = a[0][0];
+		for(i=0;i<r;i++)
+		{
+			if(colmax<a[i][j])
+			colmax=a[i][j];
+		//	colsum=colsum+a[j][i];
+		}
+		printf("%d",colmax);
+    	printf("
+");
+	}
+}
