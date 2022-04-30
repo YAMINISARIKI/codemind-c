728f7cd32fba12cb80fe481633734378348166a5
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-04-30 06:16:37 +0530

    #include<stdio.h> int main() {     int a,b,i,c;     scanf("%d%d",&a,&b);     for(i=1;i<=b;i++)     {         c=a*i;         if(c%b==0)         {             printf("%d",c);             break;         }     } }

--- LCM.c
+++ LCM.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-  int a,b,i=1;
-  scanf("%d%d",&a,&b);
-  for(;i;i++)
-  {
-      if((b*i)%a==0)
-      {
-          printf("%d",b*i);
-          break;
-      }
-  }
-  return 0;
+    int a,b,i,c;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=b;i++)
+    {
+        c=a*i;
+        if(c%b==0)
+        {
+            printf("%d",c);
+            break;
+        }
+    }
 }
