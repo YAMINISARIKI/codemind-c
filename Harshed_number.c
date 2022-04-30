0be0fce8c765d17be8edaf210932bb9cb2f70737
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-04-30 07:20:22 +0530

    #include<stdio.h> int main() {     int n,rem,sum=0,a,i;     scanf("%d",&n);     a=n;     while(a>0)     {         rem=a%10;         sum=sum+rem;         a=a/10;     }     if(n%sum==0)     {         printf("True");     }     else     {         printf("False");     } }

--- Harshed_number.c
+++ Harshed_number.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,sum=0,r,i,a;
+    int n,rem,sum=0,a,i;
     scanf("%d",&n);
     a=n;
     while(a>0)
     {
-        r=a%10;
-        sum+=r;
+        rem=a%10;
+        sum=sum+rem;
         a=a/10;
     }
     if(n%sum==0)
