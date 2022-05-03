136622a1255bcf52098e1c5d89c86108442d5846
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-05-03 13:10:40 +0530

    #include<stdio.h> int main() {     int n,i,f=0;     scanf("%d",&n);     for(i=1;i<n;i++)     {         if(n==i*i)         {             f=1;         }     }     if(f==1)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Square_Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,f=0;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        if(n==i*i)
+        {
+            f=1;
+        }
+    }
+    if(f==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
