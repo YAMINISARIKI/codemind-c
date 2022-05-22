7b39085d784fbe8b9ca86a3ef5a9bfc3eef211da
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-05-22 23:41:59 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],m=0,i;     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             m=arr[i];         }     }     printf("%d",m); }

---
+++ Last_even_number_in_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],m=0,i;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            m=arr[i];
+        }
+    }
+    printf("%d",m);
+}
