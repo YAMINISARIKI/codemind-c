08c8139092a702938499638ad8c1c8e621cd247f
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-05-22 23:28:48 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i,index=0;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             index=i;         }     }     printf("%d",index);     return 0; }

---
+++ Last_even_element_index.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i,index=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            index=i;
+        }
+    }
+    printf("%d",index);
+    return 0;
+}
