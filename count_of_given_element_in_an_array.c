dcec0868b7dbbe8d5fe9172e56c36108506b57c3
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-05-25 08:19:57 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int c=0,x;     scanf("%d",&x);     for(i=0;i<n;i++)     {         if(a[i]==x)         {             c+=1;         }     }     printf("%d",c); }

---
+++ count_of_given_element_in_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int c=0,x;
+    scanf("%d",&x);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==x)
+        {
+            c+=1;
+        }
+    }
+    printf("%d",c);
+}
