be6afa0b3e6d07c88fe50de7c606a9ef461cdc03
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-05-25 08:01:04 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int x;     scanf("%d",&x);     int c=0;     for(i=0;i<n;i++)     {         if(a[i]==x)         {             c=1;             break;         }     }     if(c==1)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int x;
+    scanf("%d",&x);
+    int c=0;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==x)
+        {
+            c=1;
+            break;
+        }
+    }
+    if(c==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
