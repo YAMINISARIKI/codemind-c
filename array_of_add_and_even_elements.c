c3375d3cd1e2361cbb5f0cc3c7101904c5ed72c2
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-06-27 23:28:14 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(int i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             printf("%d ",arr[i]);         }     }     for(int i=0;i<n;i++)     {         if(arr[i]%2==0)         {             printf("%d ",arr[i]);         }     } }

--- array_of_add_and_even_elements.c
+++ array_of_add_and_even_elements.c
@@ -3,23 +3,23 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int a[n],i;
-    for(i=0;i<n;i++)
+    int arr[n];
+    for(int i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&arr[i]);
     }
-    for(i=0;i<n;i++)
+    for(int i=0;i<n;i++)
     {
-        if(a[i]%2!=0)
+        if(arr[i]%2!=0)
         {
-            printf("%d ",a[i]);
+            printf("%d ",arr[i]);
         }
     }
-    for(i=0;i<n;i++)
+    for(int i=0;i<n;i++)
     {
-        if(a[i]%2==0)
+        if(arr[i]%2==0)
         {
-            printf("%d ",a[i]);
+            printf("%d ",arr[i]);
         }
     }
 }
