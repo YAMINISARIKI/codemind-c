812c840cb4f9edb232fe1794d1b1b72669af80ea
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-06-27 23:38:45 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,c=0;     for (i=0; i<n; i++)     {         scanf("%d",&a[i]);     }     for (i=1; i<n-1; i++)     {         if (a[i-1]%2==0 && a[i+1]%2==0 && a[i]%2==0)         {             c++;         }     }     printf("%d",c); }

--- count_even_numbers_between_even_numbers.c
+++ count_even_numbers_between_even_numbers.c
@@ -3,17 +3,17 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int a[n],i,count=0;
-    for(i=0;i<n;i++)
+    int a[n],i,c=0;
+    for (i=0; i<n; i++)
     {
         scanf("%d",&a[i]);
     }
-    for(i=0;i<n-1;i++)
+    for (i=1; i<n-1; i++)
     {
-        if(a[i-1]%2==0 && a[i+1]%2==0 && a[i]%2==0)
+        if (a[i-1]%2==0 && a[i+1]%2==0 && a[i]%2==0)
         {
-            count++;
+            c++;
         }
     }
-    printf("%d",count);
+    printf("%d",c);
 }
