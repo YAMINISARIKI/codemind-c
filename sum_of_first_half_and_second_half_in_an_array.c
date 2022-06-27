1717696c7f0a83fa2f7f2233bba17fd4c703c995
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-06-27 22:38:51 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,sum1=0,sum2=0,mid=n/2;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<mid;i++)     {         sum1=sum1+a[i];     }     for(i=mid;i<n;i++)     {         sum2=sum2+a[i];     }     printf("%d %d",sum1,sum2); }

--- sum_of_first_half_and_second_half_in_an_array.c
+++ sum_of_first_half_and_second_half_in_an_array.c
@@ -3,22 +3,19 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int a[n],i;
+    int a[n],i,sum1=0,sum2=0,mid=n/2;
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
-    int sum1=0,sum2=0,diff,mid=n/2;
     for(i=0;i<mid;i++)
     {
-        sum1+=a[i];
+        sum1=sum1+a[i];
     }
     for(i=mid;i<n;i++)
     {
-        sum2+=a[i];
+        sum2=sum2+a[i];
     }
     printf("%d
-",sum1);
-    printf("%d
-",sum2);
+%d",sum1,sum2);
 }
