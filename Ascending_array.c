34eb43c90438c402a561394144771c764fb2b081
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-06-27 23:30:26 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int c=0;     for(int i=0;i<n-1;i++)     {         if(a[i]<a[i+1])         {             c=1;         }         else         {             c=0;             break;         }     }     if(c==1)     {         printf("yes");     }     else     {         printf("no");     } }

---
+++ Ascending_array.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int c=0;
+    for(int i=0;i<n-1;i++)
+    {
+        if(a[i]<a[i+1])
+        {
+            c=1;
+        }
+        else
+        {
+            c=0;
+            break;
+        }
+    }
+    if(c==1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
