b8d748fbf9ec34c9025381a02221734385a50504
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-06-28 07:34:00 +0530

    #include<stdio.h> int main() {     char str[1000];     scanf("%[^ ]s",str);     int c=1,i;     for (i=0; str[i]!=NULL; i++)     {         if (str[i]==32)         {             c++;         }     }     printf("%d",c); }

---
+++ words_in_a_string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char str[1000];
+    scanf("%[^
+]s",str);
+    int c=1,i;
+    for (i=0; str[i]!=NULL; i++)
+    {
+        if (str[i]==32)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
