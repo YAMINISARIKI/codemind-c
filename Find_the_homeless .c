929227f36645ce2daab6e2f227b5c4242952f7d4
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-07-02 22:16:12 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],b[n],i,j,c,count=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(j=0;j<n;j++)     {         scanf("%d",&b[j]);     }     for(i=0;i<n;i++)     {         c=1;         for(j=0;j<n;j++)         {             if(a[i]<=b[j])             {                 b[j]=0;                 c=0;                 break;             }         }         if(c!=0)         {             count++;         }     }     printf("%d",count); }

---
+++ Find_the_homeless .c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],b[n],i,j,c,count=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(j=0;j<n;j++)
+    {
+        scanf("%d",&b[j]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=1;
+        for(j=0;j<n;j++)
+        {
+            if(a[i]<=b[j])
+            {
+                b[j]=0;
+                c=0;
+                break;
+            }
+        }
+        if(c!=0)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
