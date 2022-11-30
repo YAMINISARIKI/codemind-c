fd38af478c0b221cb8d2c85e33d97799bed46922
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-11-30 22:56:24 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n-i;j++)         {             printf(" ");         }         for(k=1;k<=n;k++)         {             if(i==1 || i==n || k==1 || k==n)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,i,j,k;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<i+1;j++)
+        for(j=1;j<=n-i;j++)
         {
-            printf("%d",j);
+            printf(" ");
+        }
+        for(k=1;k<=n;k++)
+        {
+            if(i==1 || i==n || k==1 || k==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
